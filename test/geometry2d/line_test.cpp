4e3bf83100542756357e23d006cca7e7547f66ff
ge59qat <ge59qat@mytum.de>
2022-05-25 19:53:14 +0200

    add another cmake

---
+++ test/geometry2d/line_test.cpp
@@ -0,0 +1,16 @@
+#include "line.h"
+#include "point.h"
+
+void testCreateLineExpectTrue(){
+    Point start;
+    start.x = 1.0;
+    start.y = 2.0;
+
+    Angle angle;
+    angle.value = 60;
+
+    distance = 50
+
+    const bool result = createLine(start, angle, distance)
+    assert(result == )
+}
